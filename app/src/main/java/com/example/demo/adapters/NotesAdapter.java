376bdada9d1fa63f7a0b2c19a0778025da25b50d
Mercymuhonja <muhonjamercy63@gmail.com>
2019-06-20 00:24:11 +0300

    demo

---
+++ app/src/main/java/com/example/demo/adapters/NotesAdapter.java
@@ -0,0 +1,38 @@
+package com.example.demo.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.demo.R;
+import com.example.demo.database.Note;
+
+import java.util.List;
+
+public class NotesAdapter extends ArrayAdapter<Note> {
+    public NotesAdapter(@NonNull Context context, int resource, @NonNull List<Note> notes) {
+        super(context, resource, notes);
+    }
+
+
+
+    @NonNull
+    @Override
+    public View getView(int position,  @Nullable View convertView,  @NonNull ViewGroup parent) {
+        Note note=getItem(position);
+        if (convertView==null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.note_list_item,parent,false);
+
+        }
+        TextView tvTitle=convertView.findViewById(R.id.tvTitle);
+        TextView tvNoteText=convertView.findViewById(R.id.tvNoteText);
+        tvTitle.setText(note.getTitle());
+        tvNoteText.setText(note.getTitle());
+        return  convertView;
+    }
+}
